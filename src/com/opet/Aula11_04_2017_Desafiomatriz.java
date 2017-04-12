7f68fb6c80db12f9d35c6ba0110980b970cef4ab
Tiago Oliveira <tiago@econtabil.com>
2017-04-11 22:02:22 -0300

    Criado somente matriz. Desenvolver todo o resto

--- src/com/opet/Aula11_04_2017_Desafiomatriz.java
+++ src/com/opet/Aula11_04_2017_Desafiomatriz.java
@@ -5,7 +5,16 @@ public class Aula11_04_2017_Desafiomatriz
 
     public static void main(String[] args)
     {
-        // TODO Auto-generated method stub
+        int [][]matX = new int[4][4];
+        matX[0][0] = 12;
+        matX[0][1] = 13;
+        matX[0][2] = 17;
+        matX[0][3] = 17;
+
+        matX[1][0] = 9;
+        matX[1][1] = 0;
+        matX[1][2] = 15;
+        matX[1][3] = 15;
 
     }
 
