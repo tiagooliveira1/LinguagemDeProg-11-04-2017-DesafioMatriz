0334ee70c51f3f3023f494d3c9662a9229f3e7e8
Tiago Oliveira <tiago@econtabil.com>
2017-04-11 22:04:00 -0300

    Criado somente matriz. Desenvolver todo o resto

--- src/com/opet/Aula11_04_2017_Desafiomatriz.java
+++ src/com/opet/Aula11_04_2017_Desafiomatriz.java
@@ -6,15 +6,25 @@ public class Aula11_04_2017_Desafiomatriz
     public static void main(String[] args)
     {
         int [][]matX = new int[4][4];
-        matX[0][0] = 12;
-        matX[0][1] = 13;
-        matX[0][2] = 17;
-        matX[0][3] = 17;
+        matX[0][0] = 1;
+        matX[0][1] = 2;
+        matX[0][2] = 3;
+        matX[0][3] = 4;
 
-        matX[1][0] = 9;
-        matX[1][1] = 0;
-        matX[1][2] = 15;
-        matX[1][3] = 15;
+        matX[1][0] = 5;
+        matX[1][1] = 6;
+        matX[1][2] = 7;
+        matX[1][3] = 8;
+
+        matX[2][0] = 9;
+        matX[2][1] = 10;
+        matX[2][2] = 11;
+        matX[2][3] = 12;
+
+        matX[3][0] = 13;
+        matX[3][1] = 14;
+        matX[3][2] = 15;
+        matX[3][3] = 16;
 
     }
 
